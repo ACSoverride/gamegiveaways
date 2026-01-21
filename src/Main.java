18ef2c6e3b47d09dc60ae164d4f684a8af95fff4
Matti <mattinhvt@gmail.com>
2026-01-21 11:53:41 -0500

    continued work on project

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import java.net.*;
 public class Main {
   public static void main(String[] args) {
-    
+      Api.LivePC();
   }
+}
--- src/api.java
+++ src/api.java
@@ -1,8 +1,11 @@
-java import java.net.http
+import java.net.http.HttpRequest;
+import java.net.URI;
+//wrapper for the game power giveaway api
 class Api {
-
-  public static string LivePC() {
-    
-  }
-  
+    //static class to get pc giveaways
+    public static String LivePC() {
+	HttpRequest req = HttpRequest.newBuilder().uri(new URI("https://gamerpower.com/api/giveaways?platform=pc")).GET();
+	System.out.println(req);
+	return "test";
+    } 
 }

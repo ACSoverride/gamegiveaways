4c730a51fc517cf7761101867d2c3643f22fc8a9
Matti <mattinhvt@gmail.com>
2026-01-21 19:42:40 -0500

    started adding maven not working at all

--- src/main/java/com/matti/app/api.java
+++ src/main/java/com/matti/app/api.java
@@ -4,8 +4,6 @@ import java.net.URI;
 class Api {
     //static class to get pc giveaways
     public static String LivePC() {
-	HttpRequest req = HttpRequest.newBuilder().uri(new URI("https://gamerpower.com/api/giveaways?platform=pc")).GET();
-	System.out.println(req);
 	return "test";
     } 
 }
